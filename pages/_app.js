bf93c294024a3fe0cd1e6e286e859158e2e9abc8
Yongchan <sdc00039@gmail.com>
2021-05-15 10:53:47 +0900

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import React from 'react';
+import { useRouter } from 'next/router'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp() {
+  render()
+  const {Component, ...other} = this.props;
+  const router = useRouter()
+
+  /*  function btnclick(){
+    const router = userouter()
+    router.push('/about')
+  } */
+
+  return(
+    <div>
+      <p>처음 시작하는 html 코드 테스트</p>
+      <button onclick={() => router.push('/about')}>버튼입니다.</button>
+      <Component {...other} />
+    </div>
+  )
 }
 
 export default MyApp
---
+++ public/component/Top.js
@@ -0,0 +1,7 @@
+export default function Top(){
+    return(
+        <div>
+            Head부분입니다.
+        </div>
+    );
+}
